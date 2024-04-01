7cd32679ecd86b2da034a91d1625e87088916ff5
Patrick Mollman <pkmollman@gmail.com>
2024-04-01 12:41:00 -0400

    nevermind, so dumb

--- betterstack/betterstack.go
+++ betterstack/betterstack.go
@@ -58,7 +58,7 @@ func (b *BetterStackClient) CreateIncident(escalation_policy, contact_email, inc
 		EscalationPolicyId string `json:"policy_id"`
 	}
 
-	betterStackIncident.RequesterEmail = "Nagios BetterStack Connector"
+	betterStackIncident.RequesterEmail = contact_email
 	betterStackIncident.IncidentName = incidentName
 	betterStackIncident.Summary = incidentCause
 	betterStackIncident.Description = incidentCause
